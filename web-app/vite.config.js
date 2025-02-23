043b4b7029ef79337c4e6ea48c9327a75548a7c4
“Siriyakorn348-2” <160942703+Siriyakorn348-2@users.noreply.github.com>
2025-02-24 01:48:20 +0700

    update

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Web-Mobile-Application-Sec2/", 
+  base: "/Web-Mobile-Application-Sec2", 
 })
