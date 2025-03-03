8600d11b54337f5a6c2b6e89d23e1e1da72697c7
“sroithongthae_3466” <sroithongthae.a@kkumail.com>
2025-03-03 14:54:28 +0700

    Update firebase.js

--- web-app/src/firebase/firebase.js
+++ web-app/src/firebase/firebase.js
@@ -1,15 +1,7 @@
-<<<<<<<< HEAD:Mobileapp/Mobileapp/components/firebase/firebase.js
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getDatabase } from 'firebase/database';
 
-========
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
->>>>>>>> main:web-app/src/firebase/firebase.js
 
 const firebaseConfig = {
   apiKey: "AIzaSyAK7O2L6dWCEMcgMHz2Mt9xxvxcN03a4zI",
@@ -25,10 +17,6 @@ const firebaseConfig = {
 // ✅ เรียก initializeApp() เพื่อสร้าง Firebase app
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-<<<<<<<< HEAD:Mobileapp/Mobileapp/components/firebase/firebase.js
-const db = getDatabase(app);  
-========
 const db = getDatabase(app);
->>>>>>>> main:web-app/src/firebase/firebase.js
 
 export { auth, db };
